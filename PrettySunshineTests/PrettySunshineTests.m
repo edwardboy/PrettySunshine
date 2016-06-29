204a882fa3632f465255f208d78c1e2220c7926c
edwardboy <1254339809@qq.com>
2016-06-29 09:23:55 +0800

    Initial Commit

---
+++ PrettySunshineTests/PrettySunshineTests.m
@@ -0,0 +1,39 @@
+//
+//  PrettySunshineTests.m
+//  PrettySunshineTests
+//
+//  Created by Groupfly on 16/6/29.
+//  Copyright © 2016年 Groupfly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PrettySunshineTests : XCTestCase
+
+@end
+
+@implementation PrettySunshineTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
